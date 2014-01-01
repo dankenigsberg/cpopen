69408ea44b7b5128887eb8c24d7d9c3ef87ab03e
Dan Kenigsberg <danken@redhat.com>
2014-01-01 02:42:47 +0000

    suppress an irrelevant warning
    Yes, we know that an evil tester can fail the test by being quicker and
    faking the mode of the temp name retuned by os.tempnam. And no, it's not
    a security risk.

--- tests/tests.py
+++ tests/tests.py
@@ -25,6 +25,7 @@ from nose.plugins.skip import SkipTest
 import signal
 import threading
 import time
+import warnings
 
 from unittest import TestCase
 
@@ -179,6 +180,9 @@ class TestCPopen(TestCase):
         self.assertEquals(out[0].strip(), '0007')
 
     def testUmaskTmpfile(self):
+        warnings.filterwarnings(
+            'ignore',
+            '^tempnam is a potential security risk.*')
         name = os.tempnam()
         p = CPopen(['touch', name], childUmask=0o007)
         p.wait()
